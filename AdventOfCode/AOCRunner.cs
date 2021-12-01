e320ffa73f57b4467d652ce0acdaec9b97062aaa
Felix Andersson Johansson <felix.andersson.johansson@prevas.se>
2021-12-01 08:49:18 +0100

    added runner class

---
+++ AdventOfCode/AOCRunner.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode._2021;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class AOCRunner
+{
+    public AOCRunner() { }
+
+    public void Run()
+    {
+        var day1 = new Day1();
+        day1.Part1();
+        day1.Part2();
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,35 +3,25 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 
-namespace AdventOfCode
+
+class Program
 {
-    class Program
+    public static void Configure() 
     {
-        public void RunAOC()
-        {
-            var day1 = new Day1();
-            day1.Part1();
-            day1.Part2();
+        var conf = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false).Build();
 
+        var url = conf.GetSection("Configuration:BaseURL").Value;
+        var name = conf.GetSection("Configuration:CookieName").Value;
+        var val = conf.GetSection("Configuration:CookieValue").Value;
+        DataFetcher.Init(url, name, val);
+    }
 
-        }
-
-        public static void Configure() 
-        {
-            var conf = new ConfigurationBuilder()
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("appsettings.json", optional: false).Build();
-
-            var url = conf.GetSection("Configuration:BaseURL").Value;
-            var name = conf.GetSection("Configuration:CookieName").Value;
-            var val = conf.GetSection("Configuration:CookieValue").Value;
-            DataFetcher.Init(url, name, val);
-        }
-
-        static void Main(string[] args)
-        {
-            Configure();
-            new Program().RunAOC();
-        }
+    static void Main(string[] args)
+    {
+        Configure();
+        new AOCRunner().Run();
     }
 }
+
