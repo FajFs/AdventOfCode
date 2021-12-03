5647a65212dda5714f12a5ca547b172b6f7023d5
Felix Andersson Johansson <felix.andersson.johansson@prevas.se>
2021-12-03 12:23:35 +0100

    Some refactoring and added day 4 template

--- AdventOfCode/2021/Day3.cs
+++ AdventOfCode/2021/Day3.cs
@@ -6,7 +6,7 @@ public class Day3
 {
     public Day3() => DataFetcher.GetAndStoreData(2021, 3);
 
-    private int[] getOnes(List<string> d)
+    private int[] GetSumOfPositiveBits(List<string> d)
     {
         var ones = new int[d.First().Length];
         foreach (var e in d)
@@ -15,17 +15,26 @@ public class Day3
         return ones;
     }
 
+    private char GetBit(int noOnes, int length, bool floorCheck = true) 
+        => floorCheck switch
+        {
+            true => noOnes >= length / 2 ? '1' : '0',
+            false => noOnes > length / 2 ? '1' : '0'
+        };
+
+
+
     public Day3 Part1()
     {
-        var d = DataFetcher.ParseDataAsStrings("\n");
+        var binLines = DataFetcher.ParseDataAsStrings("\n");
         var gammaRate = "";
         var epsilonRate = "";
 
-        var ones = getOnes(d);
-        foreach (var one in ones)
+        var filter = GetSumOfPositiveBits(binLines);
+        foreach (var noOnes in filter)
         {
-            gammaRate += one > d.Count / 2 ? "1" : "0";
-            epsilonRate += one > d.Count / 2 ? "0" : "1";
+            gammaRate += GetBit(noOnes, binLines.Count);
+            epsilonRate += GetBit(binLines.Count - noOnes, binLines.Count);
         }
         Console.WriteLine($"Part 1: {Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2)}");
         return this;
@@ -33,23 +42,23 @@ public class Day3
 
     public Day3 Part2()
     {
-        var oxygen = DataFetcher.ParseDataAsStrings("\n");
-        var co2 = DataFetcher.ParseDataAsStrings("\n");
+        var oxygenBinLines = DataFetcher.ParseDataAsStrings("\n");
+        var co2BinLines = DataFetcher.ParseDataAsStrings("\n");
 
-        for(var i = 0; i < oxygen.First().Length; i++)
+        for(var i = 0; i < oxygenBinLines.First().Length; i++)
         {
-            if (oxygen.Count == 1) break;
-            var val = getOnes(oxygen)[i] >= oxygen.Count / 2 ? '1' : '0';
-            oxygen = oxygen.Where(line => line[i] == val).ToList();
+            if (oxygenBinLines.Count == 1) break;
+            var filter = GetBit(GetSumOfPositiveBits(oxygenBinLines)[i], oxygenBinLines.Count);
+            oxygenBinLines = oxygenBinLines.Where(binLine => binLine[i] == filter).ToList();
         }
         
-        for (var i = 0; i < co2.First().Length; i++)
+        for (var i = 0; i < co2BinLines.First().Length; i++)
         {
-            if (co2.Count == 1) break;
-            var val = getOnes(co2)[i] >= co2.Count / 2 ? '0' : '1';
-            co2 = co2.Where(line => line[i] == val).ToList();
+            if (co2BinLines.Count == 1) break;
+            var filter = GetBit(co2BinLines.Count - GetSumOfPositiveBits(co2BinLines)[i], co2BinLines.Count, floorCheck: false);
+            co2BinLines = co2BinLines.Where(binLine => binLine[i] == filter).ToList();
         }
-        Console.WriteLine($"Part 1: {Convert.ToInt32(oxygen.First(), 2) * Convert.ToInt32(co2.First(), 2)}");
+        Console.WriteLine($"Part 1: {Convert.ToInt32(oxygenBinLines.First(), 2) * Convert.ToInt32(co2BinLines.First(), 2)}");
         return this;
     }
 }
---
+++ AdventOfCode/2021/Day4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Day4
+{
+    public Day4() => DataFetcher.GetAndStoreData(2021, 4);
+
+    public Day4 Part1()
+    {
+
+        return this;
+    }
+
+    public Day4 Part2()
+    {
+
+        return this;
+    }
+}
