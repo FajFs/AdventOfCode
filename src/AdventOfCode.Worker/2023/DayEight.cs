5eac4b4857bfe2e5b11699460453c2d4c7a284c8
Felix Andersson Johansson <felix.andersson.johansson@prevas.se>
2023-12-08 13:54:41 +0100

    day 8 & prepare day 9

--- src/AdventOfCode.Worker/2023/DayEight.cs
+++ src/AdventOfCode.Worker/2023/DayEight.cs
@@ -1,5 +1,8 @@
 ﻿using AdventOfCode.Common;
 using AdventOfCode.Worker.Interfaces;
+using System.Collections.Frozen;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace AdventOfCode.Worker;
 
@@ -11,17 +14,106 @@ public partial class DayEight(
     private readonly IInputRepository _inputRepository = inputRepository ?? throw new ArgumentNullException(nameof(inputRepository));
     private readonly ILogger<DayEight> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
+
+    [GeneratedRegex(@"^(\w+) = \((\w+), (\w+)\)$")]
+    private static partial Regex ParseNode();
+    private record Node(string Name, string LeftChild, string RightChild );
+
     public async Task Part1()
     {
         await _inputRepository.GetInputAsync(year: 2023, day: 8);
+        var instructions = _inputRepository.ToList<string>("\n").First();
 
-        //_logger.LogInformation("Part 1: {result}", result);
+        var nodes = _inputRepository.ToList<string>("\n")
+            .Skip(1)
+            .Select(x =>
+            {
+                var match = ParseNode().Match(x);
+                return new Node(
+                    Name: match.Groups[1].Value,
+                    LeftChild: match.Groups[2].Value,
+                    RightChild: match.Groups[3].Value);
+            })
+            .ToFrozenSet();
+
+        var currentNode = nodes.First(x => x.Name == "AAA");
+        var steps = 0;
+        while (currentNode.Name != "ZZZ")
+        {
+            foreach (var instruction in instructions)
+            {
+                steps++;
+                if (instruction == 'L')
+                    currentNode = nodes.First(x => x.Name == currentNode.LeftChild);
+                else
+                    currentNode = nodes.First(x => x.Name == currentNode.RightChild);
+
+                if (currentNode.Name == "ZZZ")
+                {
+                    _logger.LogInformation("Part 1: {result}", steps);
+                    return;
+                }
+            }
+        }
+    }
+
+    private long GetStepsUntilGhostStops(Node currentNode, string instructions, FrozenSet<Node> map)
+    {
+        var _nodes = new HashSet<string>();
+        long cycle = 0;
+        while (true)
+        {
+            foreach (var instruction in instructions)
+            {
+                cycle++;
+                if (instruction == 'L')
+                    currentNode = map.First(x => x.Name == currentNode.LeftChild);
+                else
+                    currentNode = map.First(x => x.Name == currentNode.RightChild);
+                if (currentNode.Name.EndsWith('Z'))
+                    return cycle;
+            }
+        }
     }
 
     public async Task Part2()
     {
         await _inputRepository.GetInputAsync(year: 2023, day: 8);
+        var instructions = _inputRepository.ToList<string>("\n")
+           .First();
 
-        //_logger.LogInformation("Part 2: {result}", result);
+        var nodes = _inputRepository.ToList<string>("\n")
+            .Skip(1)
+            .Select(x =>
+            {
+                var match = ParseNode().Match(x);
+                return new Node(
+                    Name: match.Groups[1].Value,
+                    LeftChild: match.Groups[2].Value,
+                    RightChild: match.Groups[3].Value);
+            })
+            .ToFrozenSet();
+
+        var stepsToReachEnd = nodes
+            .Where(x => x.Name.EndsWith('A'))
+            .Select(x => GetStepsUntilGhostStops(x, instructions, nodes)).ToList();
+
+        //calculate LCM
+        var lcm = stepsToReachEnd.Aggregate((a, b) => (a / Gcf(a, b)) * b);
+
+        _logger.LogInformation("Part 2: {result}", lcm);
+
+    }
+
+    //helper method to calculate LCM
+    private static long Gcf(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
---
+++ src/AdventOfCode.Worker/2023/DayNine.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Worker.Interfaces;
+
+namespace AdventOfCode.Worker;
+
+public partial class DayNine(
+    ILogger<DayNine> logger,
+    IInputRepository inputRepository)
+    : IDay
+{
+    private readonly IInputRepository _inputRepository = inputRepository ?? throw new ArgumentNullException(nameof(inputRepository));
+    private readonly ILogger<DayNine> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public async Task Part1()
+    {
+        await _inputRepository.GetInputAsync(year: 2023, day: 9);
+
+        //_logger.LogInformation("Part 1: {result}", result);
+    }
+
+    public async Task Part2()
+    {
+        await _inputRepository.GetInputAsync(year: 2023, day: 9);
+
+        //_logger.LogInformation("Part 2: {result}", result);
+    }
+}
